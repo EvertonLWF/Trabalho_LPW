35e096e40b2564f7847731fb0e2f5901b7d706b3
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-06-18 18:13:50 -0300

    Update login.php

--- blog/login.php
+++ blog/login.php
@@ -1,4 +1,21 @@
 <?php
+require_once ('conexao.php');
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
+$sql = "SELECT id_user,nome FROM usuarios WHERE email='".$email."' "AND" senha='".$senha."'";
+$res = pg_query($consulta , $sql);
+if(pg_num_rows ($res) > 0){
+	$dados = pg_fetch_assoc($res);
+	echo var_dump($dados);
+    $_SESSION['id'] = $dados['id_user'];
+	$_SESSION['nome'] = $dados['nome'];
+    header("Location:index.php");
+} else{
+	header("Location:cadastro.php");
+}
+echo $_SESSION['nome'];
+echo "<br/>";
+echo '<a href="logout.php">Encerrar sessao</a>';
 
 ?>
