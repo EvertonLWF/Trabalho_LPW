46273d829bcf9c339e103d62d5312abfa9d8f42e
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-06-18 18:12:30 -0300

    Update conexao.php

--- blog/conexao.php
+++ blog/conexao.php
@@ -4,7 +4,8 @@ session_start();
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-	$consulta=pg_connect("host=localhost port=5432 dbname=blog user=root");
+	$consulta=pg_connect("host=localhost port=5432 dbname=blog user=postgres password=senha5");//banco com postgres
+// 	$consulta=mysqli_connect("host=localhost port=5432 dbname=blog user=root");// banco com mysql
 	if($consulta==true){
 		echo "Conectado ao Banco de dados";
 	}
