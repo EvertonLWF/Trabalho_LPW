66f7ff3900a70860473a6e108ef59a401014dd37
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-06-20 17:50:39 -0300

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -3,12 +3,41 @@ require_once("cabeçalho.php");
 
 
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+	</form>
+	
 
 </body>
 </html>
 
+<?php
 
+	require_once("footer.php");
 
-
-
+?>
