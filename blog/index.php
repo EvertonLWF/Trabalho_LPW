8d1a02f4109ea52d1fcdc6012fd390543e178c10
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-06-25 13:25:29 -0300

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -1,36 +1,36 @@
-<?php 
-  require_once("conexao.php");
-  echo "teste cabecalho OK<br>";
-  if(isset($_SESSION['nome'])){
-            echo "<br>Seja bem vindo ";
-            echo $_SESSION['nome'];
-            ?>  <div><form  action="logout.php"><button type="input">Logout</button></form></div> <?php
-
-  }else{
+<?php
+require_once("cabecalho.php");
 ?>
-<form action="login.php" method="POST">
-  Digite seu e-mail:
-  <input type="email" name="email">
-  Digite sua senha:
-  <input type="password" name="senha">
-  <input type="submit" value="login">
-</form>
-<form action="cadastro.php">
-    <button type="input">Cadastro</button>
-</form>
 
-<?php }?>
+	<form>
+		<fieldset style="background-color: blue">
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+		<fieldset>
+			<h3>
+				Espaço reservado para feeds de ultimas noticias!!!!
+			</h3>
+		</fieldset>
+	</form>
+	
 
+</body>
+</html>
 
+<?php
 
-<fieldset>
-  <h4>
-    Espaço reservado para marcadores!!!!
-  </h4>
-</fieldset>
-<br>
-<form action="busca.php" method="post">
-  <label for="busca">Buscar</label>
-  <input align="center" size="100" type="search" id="busca" name="q">
-  <button type="submit">OK</button>
-</form>
+	require_once("footer.php");
+
+?>
