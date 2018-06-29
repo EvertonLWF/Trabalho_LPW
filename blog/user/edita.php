5ef677ba3ea5b831b5f0502ce97de0a7b8c86eb7
Lucas Hinz de Souza <36297261+lucasbkk@users.noreply.github.com>
2018-06-29 08:53:46 -0300

    Update edita.php

--- blog/user/edita.php
+++ blog/user/edita.php
@@ -1,1 +1,15 @@
-<?php ?>
+<?php
+session_start();
+require_once('../conexao.php');
+$id 		= $_POST['id'];
+$titulo 	= $_POST['titulo'];
+$descricao 	= $_POST['descricao'];
+$data 		= $_POST['data'];
+$sql = 'UPDATE posts SET titulo = \''.$titulo.'\', descricao = \''.$descricao.'\', data = \''.$data.'\' WHERE id = '.$id;
+$res = mysqli_query($conexao, $sql);
+if($res != false){
+	if(mysqli_affected_rows($res) != 0){
+		header('Location: post.php?id='.$id);
+	}
+}
+?>
