509b7900a6c670e01c220d20d01f9855e7ff84d0
Lucas Hinz de Souza <36297261+lucasbkk@users.noreply.github.com>
2018-06-29 09:22:12 -0300

    Update deleta.php

--- blog/user/deleta.php
+++ blog/user/deleta.php
@@ -1,1 +1,19 @@
-<?php ?>
+ <?php 
+	require_once('../conexao.php');
+	$id_post=$_GET['id_post'];
+	$id_user=$_GET['id_user'];
+	$_SESSION['id_user']=$id_user;
+	if(isset($_GET['id_post'])&&!empty($_GET['id_post'])){
+		$sql="DELETE FROM posts WHERE id_post='".$id_post."'";
+		$res=mysqli_query($consulta,$sql);
+
+		header("location:user/index.php");
+	}else{
+		echo "erro!!!!!!!!";
+	}
+
+?>
+
+
+
+
