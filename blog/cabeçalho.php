bde18c48b157873dfe63c12fedae84fc08a46d1f
renatasaraivav <36297349+renatasaraivav@users.noreply.github.com>
2018-06-29 08:43:52 -0300

    Update cabeçalho.php

--- blog/cabeçalho.php
+++ blog/cabeçalho.php
@@ -6,7 +6,47 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="style.css">
+  <script language="javascript" type="text/javascript">
+    function validarlogin() {
+      var email = form2.email.value;
+      var senha = form2.senha.value;
 
+      if(email == "" || email.indexOf('@') == -1){
+        alert('Preencha o campo com seu e-mail')
+        form2.email.focus();
+        return false;
+      }
+      if(senha == ""){
+        alert('Preencha o campo com sua senha')
+        form2.senha.focus();
+        return false;
+      }
+    };
+
+    function validarcadastro() {
+      var nome = form1.nome.value;
+      var email = form1.email.value;
+      var dtnasc = form1.dtnasc.value;
+      var senha = form1.senha.value;
+
+      if(nome == ""){
+      alert('Preencha o campo com seu nome.')
+      form1.nome.focus();
+      return false;
+      }
+      if(email == "" || email.indexOf('@') == -1){
+      alert('Preencha o campo com seu e-mail.')
+      form1.email.focus();
+      return false;
+      }
+
+      if(senha == "" || senha.length < 6){
+      alert('Preencha o campo senha com no mínimo 6 caracteres.')
+      form1.email.focus();
+      return false;
+      }
+    };
+  </script>
 
 	<title></title>
 </head>
@@ -21,12 +61,49 @@
   </div>
 
   <div class="alinha" class="text-center">
-      <a href="novo.php" class="btn btn-light" role="button">Cadastrar</a>
+      <!--<a href="novo.php" class="btn btn-light" role="button">Cadastrar</a> -->
     
-  <!-- Button trigger modal -->
+      <button type="button" class="btn btn-light" data-toggle="modal" data-target="#cadastro">
+              Cadastre-se
+      </button>
+
       <button type="button" class="btn btn-light" data-toggle="modal" data-target="#login">
         Login
       </button>
+            <!-- Modal -->
+            <div class="modal fade" id="cadastro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cadastro</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body" class="text-center">
+                     <form action="cadastro.php" method="post" name="form1" >
+                        Nome Completo<br>
+                        <input type="text" name="nome" class="form-control"><br>
+                        E-mail<br>
+                        <input type="email" name="email" class="form-control"><br>
+                        Data de Nascimento<br>
+                        <input type="date" name="dtnasc" class="form-control"><br>
+                        Senha<br>
+                        <input type="password" name="senha" class="form-control"><br><br>
+                        <br>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                          <button type="reset" class="btn btn-primary">Limpar</button>
+                          <button type="submit" class="btn btn-primary" onclick="return validarcadastro();">Cadastrar</button>
+                        </div>
+                     </form>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+  <!-- Modal do Login -->
+      
       <!-- Modal -->
       <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -38,17 +115,22 @@
               </button>
             </div>
             <div class="modal-body" class="text-center">
-	<form action="login.php" method="post">
-         	Email: <input type="email" name="email" class="form-control"><br>
-        	Senha: <input type="password" name="senha" class="form-control">
-            	 <div class="modal-footer">
-             	 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-             	 <button type="button" class="btn btn-primary">Logar</button>
-            	</div>
-	</form>    
+             <form action="login.php" method="post" name="form2" >
+              Email: <input type="email" name="email" class="form-control"><br>
+              Senha: <input type="password" name="senha" class="form-control">
+              <br>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="submit" class="btn btn-primary" onclick="return validarlogin();">Logar</button>
+              </div>
+             </form>
+            </div>
+            
+          </div>
+        </div>
       </div>
-    </div>
-</div>
-</div>
-</div>
+  </div>
 </nav>
+
+
+
